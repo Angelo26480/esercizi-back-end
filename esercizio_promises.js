af3829a66c2b2eb444817b02b42d37e041457ef5
Angelo Lo Presti <angelolopresti26@gmail.com>
2024-11-21 10:09:51 +0100

    feat(adding)aggiunta esercizio promises

---
+++ esercizio_promises.js
@@ -0,0 +1,28 @@
+function order_marco(marco) {
+    return new Promise((resolve, reject) => {
+        if (!marco) {
+            reject('Ordine ricevuto per errore.');
+            return;
+        }
+
+        console.log('Ordine ricevuto per: ' + marco);
+
+        setTimeout(() => {
+            console.log('Preparazione del cibo per: ' + marco);
+            resolve(marco);
+        }, 3000);
+    })
+    .then(result => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('Consegna dell\'ordine per: ' + result);
+                resolve(result);
+            }, 1000);
+        });
+    });
+}
+
+
+order_marco('Marco')
+    .then(result => console.log('Successo:', result))
+    .catch(error => console.error('Errore:', error));
